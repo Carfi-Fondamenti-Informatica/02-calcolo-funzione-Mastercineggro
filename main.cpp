174faa2100dd1c11156146663f44048757b5d4bf
Furio Scopinich <116543040+Mastercineggro@users.noreply.github.com>
2022-11-17 08:19:28 +0000

    Fatto esercizio.

--- main.cpp
+++ main.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
+   float a,x,b,y;
+   cin >> a >> b >> x >> y;
+
+   float result;
+   if (x<0 && y>0) {
+   result = a*x -b*y;
+   }
+   else if (x>= 0 && y<=0){
+      result = a*( x*x) - b*y;
+   }
+   else{
+      result = a*x +b*(y*y);
+   }
+   cout<<result;
    return 0;
 }
